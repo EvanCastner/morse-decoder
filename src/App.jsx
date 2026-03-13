ec53b5a45e5ce9488f14df944232c6bb88fa5ad6
Evan <ekc30@uakron.edu>
2026-03-13 13:19:23 -0400

    Fix textbox size and font as well as button size and font and location

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,18 @@ function App() {
           <h1>Morse Code Decoder</h1>
         </header>
 
-        <main>
-          <input
-            type="text"
+        <main style={{ diplay: "flex", flexDirection: "column", alignItems: "center", gap: "12px" }}>
+          <textarea
             value={inputValue}
             onChange={(e) => setInputValue(e.target.value)}
             placeholder="Type in Morse Code Values"
+            style={{ fontSize: "20px", width: "1000px", height: "200px", padding: "10px", resize: "none" }}
           />
 
-          <button onClick={() => console.log(inputValue)}>
+          <button 
+            onClick={() => console.log(inputValue)}
+            style={{ fontSize: "18px", padding: "12px 30px", marginTop: "10px" }}
+          >
             Click to Decode
           </button>
         </main>
