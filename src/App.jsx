a1a6b61858641d07b092bbba09a3eac1bd5f4b8e
Evan <ekc30@uakron.edu>
2026-03-13 14:56:53 -0400

    Create morse code map

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import MorseInput from './components/MorseInput';
 import OutputDisplay from './components/OutputDisplay';
 import Controls from './components/Controls';
+import morseMap from './utils/morseMap';
 
 function App() {
   const [inputValue, setInputValue] = useState("");
